1142e5803efcd29c83842cfe648c01edb0eb4c85
millionkn <millionkn@sina.com>
2019-09-06 10:21:07 +0900

    vue functional api

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueCompositionApi from "@vue/composition-api";
 
 Vue.config.productionTip = false
-
+Vue.use(VueCompositionApi);
+declare module '@vue/composition-api/dist/component/component' {
+  interface SetupContext {
+    readonly refs: { [key: string]: Vue | Element | Vue[] | Element[] };
+  }
+}
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
