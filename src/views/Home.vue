44340a9f1da1f31829f02ea9e5ce5a3f480cf473
millionkn <millionkn@sina.com>
2019-09-15 15:43:21 +0900

    格式化文档

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
 
-export default Vue.extend({
-})
+export default Vue.extend({});
 </script>
