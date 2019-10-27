696cc3402a383ea43359d1a79330684d9f23777a
millionkn <millionkn@sina.com>
2019-10-27 12:15:25 +0900

    删除课程

--- src/views/MyLessonList/MyLessonList.ts
+++ src/views/MyLessonList/MyLessonList.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import router from "@/router";
 import { Lesson } from '@/types';
+import { deleteLesson } from '@/service';
 
 export type propMethod = () => {
   lessonList: Lesson[]
@@ -12,6 +13,10 @@ export default Vue.extend({
     return {
       lessonList: props.lessonList,
       handlerCreateLesson: async () => { await router.push(`/myLesson/create`).finally() },
+      handlerDelete: async (lesson: Lesson) => {
+        await deleteLesson(lesson);
+        props.lessonList.splice(props.lessonList.indexOf(lesson), 1)
+      },
       handlerReview: async (lesson: Lesson) => { await router.push(`/review/${lesson.id}`).finally() }
     };
   }
